2bc6f765ae1938f781a2cb61e2bb489f4d8b8342
SU-T <guitaristpeet@gmail.com>
2022-09-01 16:45:09 -0700

    install and import gsap

--- src/Float.js
+++ src/Float.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import { gsap } from "gsap";
 
 const Float = () => {
+    let top = useRef();
+    let ly1 = useRef();
+
+    useEffect(() => {
+        var tl = gsap.timeline();
+        // tl.to(top, {
+        //     y: 120,
+        //     x: -7,
+        //     duration: 2.2,
+        //     repeat: -1,
+        //     yoyo: true,
+        //     ease: "power2.inOut"
+        // })
+        // tl.to(ly1, {
+        //     y: 120,
+        //     x: -7,
+        //     duration: 2.2,
+        //     repeat: -1,
+        //     yoyo: true,
+        //     ease: "power2.inOut"
+        // }, '<0.2')
+    }, []);
+
     return (
         <>
         <main className='noise'>
             <article className='letters'>
                 <section className="letter">
-                    <h1 className='top'>f</h1>
-                    <h1 className='ly1'>f</h1>
+                    <h1 ref={el => {top = el}} className='top'>f</h1>
+                    <h1 ref={el => {ly1 = el}} className='ly1'>f</h1>
                     <h1 className='ly2'>f</h1>
                     <h1 className='ly3'>f</h1>
                     <h1 className='ly4'>f</h1>
