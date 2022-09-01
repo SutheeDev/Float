210225a3eca7fafcf89739ec8e4754e7d12e2531
SU-T <guitaristpeet@gmail.com>
2022-08-31 22:50:42 -0700

    manage letter layout

--- src/Float.js
+++ src/Float.js
@@ -3,24 +3,26 @@ import React from 'react'
 const Float = () => {
     return (
         <>
-        <article className='letters'>
-            <section className="letter">
-                <h1 className='top'>f</h1>
-                <h1 className='ly1'>f</h1>
-                <h1 className='ly2'>f</h1>
-                <h1 className='ly3'>f</h1>
-                <h1 className='ly4'>f</h1>
-                {/* <h1 className='bottom'>f</h1> */}
-            </section>
-            {/* <section className="letter"> */}
-                {/* <h1 className='top'>l</h1> */}
-                {/* <h1 className='ly1'>l</h1>
-                <h1 className='ly2'>l</h1>
-                <h1 className='ly3'>l</h1>
-                <h1 className='ly4'>l</h1>
-                <h1 className='bottom'>l</h1> */}
-            {/* </section> */}
-        </article>
+        <main>
+            <article className='letters'>
+                <section className="letter">
+                    <h1 className='top'>f</h1>
+                    <h1 className='ly1'>f</h1>
+                    <h1 className='ly2'>f</h1>
+                    <h1 className='ly3'>f</h1>
+                    <h1 className='ly4'>f</h1>
+                    <h1 className='bottom'>f</h1>
+                </section>
+                <section className="letter">
+                    <h1 className='top'>l</h1>
+                    <h1 className='ly1'>l</h1>
+                    <h1 className='ly2'>l</h1>
+                    <h1 className='ly3'>l</h1>
+                    <h1 className='ly4'>l</h1>
+                    <h1 className='bottom'>l</h1>
+                </section> 
+            </article>
+        </main>
         </>
     )
 };
