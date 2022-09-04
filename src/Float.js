ca6d04d8377595e44721f442614a5dc9c413dd21
SU-T <guitaristpeet@gmail.com>
2022-09-04 13:28:20 -0700

    another clean up

--- src/Float.js
+++ src/Float.js
@@ -9,13 +9,6 @@ function Float (){
             <Circle2/>
             <Circle3/>
             <Letters/>
-            {/* <article className='letters'>
-                <Letter letter='f'/>
-                <Letter letter='l'/>
-                <Letter letter='o'/>
-                <Letter letter='a'/>
-                <Letter letter='t'/>
-            </article> */}
         </main>
         </>
     )
@@ -70,7 +63,7 @@ const Letter = (props) => {
     let ly3 = useRef();
     let ly4 = useRef();
     let bottom = useRef();
-
+        
     useEffect(() => {
         const letterTimeline = (el) => {
             var tl = gsap.timeline();
@@ -90,22 +83,18 @@ const Letter = (props) => {
             var ly1Tl = gsap.timeline();
             ly1Tl.add(letterTimeline(ly1))
         }
-
         const ly2Tl = () => {
             var ly2Tl = gsap.timeline();
             ly2Tl.add(letterTimeline(ly2))
         }
-
         const ly3Tl = () => {
             var ly3Tl = gsap.timeline();
             ly3Tl.add(letterTimeline(ly3))
         }
-
         const ly4Tl = () => {
             var ly4Tl = gsap.timeline();
             ly4Tl.add(letterTimeline(ly4))
         }
-
         const bottomTl = () => {
             var bottomTl = gsap.timeline();
             bottomTl.add(letterTimeline(bottom))
@@ -113,42 +102,33 @@ const Letter = (props) => {
         
         var lettersTl = gsap.timeline();
         lettersTl.add(topTl)
-                .add(ly1Tl, '<0.2')
-                .add(ly2Tl, '<0.2')
-                .add(ly3Tl, '<0.2')
-                .add(ly4Tl, '<0.2')
-                .add(bottomTl, '<0.2')
+                    .add(ly1Tl, '<0.2')
+                    .add(ly2Tl, '<0.2')
+                    .add(ly3Tl, '<0.2')
+                    .add(ly4Tl, '<0.2')
+                    .add(bottomTl, '<0.2')
     }, []);
-
     return (
-    <section className="letter">
-        <h1 ref={el => {top = el}} className='top'>{props.letter}</h1>
-        <h1 ref={el => {ly1 = el}} className='ly1'>{props.letter}</h1>
-        <h1 ref={el => {ly2 = el}} className='ly2'>{props.letter}</h1>
-        <h1 ref={el => {ly3 = el}} className='ly3'>{props.letter}</h1>
-        <h1 ref={el => {ly4 = el}} className='ly4'>{props.letter}</h1>
-        <h1 ref={el => {bottom = el}} className='bottom'>{props.letter}</h1>
-    </section>
+        <section className="letter letterCol" >
+            <h1 ref={el => {top = el}} className='letterRow top'>{props.letter}</h1>
+            <h1 ref={el => {ly1 = el}} className='letterRow ly1'>{props.letter}</h1>
+            <h1 ref={el => {ly2 = el}} className='letterRow ly2'>{props.letter}</h1>
+            <h1 ref={el => {ly3 = el}} className='letterRow ly3'>{props.letter}</h1>
+            <h1 ref={el => {ly4 = el}} className='letterRow ly4'>{props.letter}</h1>
+            <h1 ref={el => {bottom = el}} className='letterRow bottom'>{props.letter}</h1>
+        </section>
     )
 };
 
 const Letters = (prop) => {
-    let f = useRef();
-    let l = useRef();
-    let o = useRef();
-    let a = useRef();
-    let t = useRef();
-
-    var masterTl = gsap.timeline();
-
     return (
         <>
             <article className='letters'>
-                <Letter ref={el => {f = el}} letter='f' delay='0'/>
-                <Letter ref={el => {l = el}} letter='l'delay='0.3'/>
-                <Letter ref={el => {o = el}} letter='o'delay='0.3'/>
-                <Letter ref={el => {a = el}} letter='a'delay='0.3'/>
-                <Letter ref={el => {t = el}} letter='t'delay='0.3'/>
+                <Letter letter='f' delay='0'/>
+                <Letter letter='l'delay='0.3'/>
+                <Letter letter='o'delay='0.3'/>
+                <Letter letter='a'delay='0.3'/>
+                <Letter letter='t'delay='0.3'/>
             </article>
         </>
     )
